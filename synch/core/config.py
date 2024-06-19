09eeb44da9c43962b335795986dae296cab28f9d
Alexey Melnikov <mas-chel@mail.ru>
2024-06-20 01:34:41 +0500

    upd

--- synch/core/config.py
+++ synch/core/config.py
@@ -1,4 +1,5 @@
 from dotenv import dotenv_values, find_dotenv
+from collections import OrderedDict
 
 from log_config import logger
 
@@ -7,4 +8,8 @@ if not find_dotenv():
     logger.error("Файл с переменными .env не найден")
     exit()
 
-config = dotenv_values(".env")
+config = OrderedDict(
+    (k.lower(), v) for k, v in dotenv_values(".env", encoding="utf-8").items()
+)
+
+print(config)
