ff541e1bf81a77f31a263fbccaeb0b3e5749b954
Alexey Melnikov <mas-chel@mail.ru>
2024-06-22 00:01:45 +0500

    added function delete in YandexDisk

--- synch/cloud_storage/yandex.py
+++ synch/cloud_storage/yandex.py
@@ -1,5 +1,7 @@
+import os
 import requests
 from requests import HTTPError, Timeout, RequestException
+from urllib.parse import quote
 
 from synch.core.config import config
 from synch.core.log_config import logger
@@ -19,18 +21,42 @@ class YandexApiUrl:
             f"{self.host}/" f"{self.version}/" f"{self.disk}/" f"{self.resources}"
         )
 
-    def get_info_url(self) -> str:
-        return (
-            f"{self.main_url}"
-            f"?path={config.get("path_cloud_folder")}"
-            f"&fields={config.get("yandex_disk_fields")}"
-        )
+    @staticmethod
+    def encode_path(path: str | None) -> str:
+        if path:
+            return quote(path)
+        return ""
 
-    def get_upload_url(self, upload: bool = False) -> str:
+    def get_info_url(self) -> str:
+        path = config.get("yandex_disk_path")
+        fields = config.get("yandex_disk_fields")
+        encoded_path = self.encode_path(path)
+        return f"{self.main_url}" f"?path={encoded_path}" f"&fields={fields}"
+
+    def get_upload_url(self, file_path: str, upload: bool = False) -> str:
         overwrite: str = "false"
         if upload:
             overwrite: str = "true"
-        return f"{self.main_url}{self.upload}" "?path={}" f"&overwrite={overwrite}"
+        encoded_path = self.encode_path(file_path)
+        return (
+            f"{self.main_url}{self.upload}"
+            f"?path={encoded_path}"
+            f"&overwrite={overwrite}"
+        )
+
+    def get_delete_url(self, file_name: str) -> str:
+        folder_path = config.get("yandex_disk_path")
+        if not folder_path:
+            folder_path = ""
+        full_path = os.path.join(folder_path, file_name)
+        encoded_path = self.encode_path(full_path)
+        permanently = config.get("yandex_disk_permanently", "false")
+        return (
+            f"{self.main_url}"
+            f"?path={encoded_path}"
+            "&force_async=false"
+            f"&permanently={permanently}"
+        )
 
 
 class YandexDisk:
@@ -45,10 +71,10 @@ class YandexDisk:
         }
 
     @handle_errors
-    def request_upload_url(self, upload: bool = False) -> str | None:
+    def request_upload_url(self, file_path: str, upload: bool = False) -> str | None:
 
         response = requests.get(
-            self.api.get_upload_url(upload),
+            self.api.get_upload_url(file_path, upload),
             headers=self.headers,
             timeout=self.api.timeout,
         )
@@ -64,7 +90,7 @@ class YandexDisk:
 
     @handle_errors
     def upload(self, file_path: str) -> dict[str, str]:
-        upload_url = self.request_upload_url()
+        upload_url = self.request_upload_url(file_path=file_path)
         if not upload_url:
             logger.error("Ошибка загрузки файла")
             return {"status": "Error"}
@@ -81,7 +107,10 @@ class YandexDisk:
 
     @handle_errors
     def reload(self, file_path: str) -> dict[str, str]:
-        reload_url = self.request_upload_url(upload=True)
+        reload_url = self.request_upload_url(
+            file_path=file_path,
+            upload=True,
+        )
         if not reload_url:
             return {"status": "Error"}
 
@@ -96,7 +125,18 @@ class YandexDisk:
         return {"status": "Success"}
 
     @handle_errors
-    def delete(self, file_name: str): ...
+    def delete(self, file_name: str):
+        delete_url = self.api.get_delete_url(file_name)
+        response = requests.delete(
+            delete_url,
+            headers=self.headers,
+            timeout=self.api.timeout,
+        )
+
+        response.raise_for_status()
+
+        logger.info(f"Файл {file_name} удален")
+        return {"status": "Success"}
 
     @handle_errors
     def get_info(self) -> dict[str, list[dict]] | None:
